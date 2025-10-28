8f96b99d886e058bfe5f50cefee78a70ab2b34fa
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2025-10-28 22:44:23 +0530

    Update API credentials and database URI in config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", "27353035"))
-API_HASH = environ.get("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-BOT_TOKEN = environ.get("BOT_TOKEN", "5843226951:AAEi-4rmnqU3gQ86Rv0hcuBFLUpdUGRhRcE")
+API_ID = int(environ.get("API_ID", "28079855"))
+API_HASH = environ.get("API_HASH", "3493b73fe86e88f409ac7b95d23cff5b")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8421516268:AAHX-hFv4zLEWv1J49bQGOD3JyA3xEScMAg")
 
 # Make Bot Admin In Log Channel With Full Rights
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002073865889"))
 ADMINS = int(environ.get("ADMINS", "1327021082"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "mongodb+srv://poulomig644_db_user:d9MMUd5PsTP5MDFf@cluster0.q5evcku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("DB_URI", "mongodb+srv://Takiusername01:Takiusername01@cluster0.4rb2rl6.mongodb.net/?appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "vjlinkchangerbot")
