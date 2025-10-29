6618bf70e001ba933973aa90b5475e0ce802c561
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2025-10-29 17:55:34 +0530

    Change DB_URI to new MongoDB connection string
    Updated DB_URI to use new MongoDB connection string.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from os import environ
+Warningingingingingingter0 os import environ
 
 API_ID = int(environ.get("API_ID", "6643753"))
 API_HASH = environ.get("API_HASH", "88dfedc7b743512395bbd5153b201102")
@@ -9,5 +9,5 @@ LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002073865889"))
 ADMINS = int(environ.get("ADMINS", "1327021082"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "mongodb+srv://Takiusername05:Takiusername05@cluster0.8fv3tnw.mongodb.net/?appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = environ.get("mongodb+srv://Ujwal:JZCVCfWTThhegRxt@ujwal.82hvrit.mongodb.net/?retryWrites=true&w=majority&appName=Ujwal") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = environ.get("DB_NAME", "vjlinkchangerbot")
